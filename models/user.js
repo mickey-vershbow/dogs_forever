ca03ca81bfd502de301d893647a721a6841a623c
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-06-24 23:53:29 -0400

    commit

--- models/user.js
+++ models/user.js
@@ -1,31 +1,35 @@
 // Import Schema and Model
-const {Schema, model} = require("../db/connection")
+const { Schema, model } = require("../db/connection");
 // The Image Schema
 const Dog = new Schema({
+  id: String,
+  name: String,
+  temperament: String,
+  breed_group: String,
+  bred_for: String,
+  life_span: String,
+  origin: String,
+  weight: {},
+  country_code: String,
+  height: {},
+  hypoallergenic: Number,
+  image: {
     id: String,
-    name: String,
-    temperament: String,
-    life_span: String,
-    wikipedia_url: String,
-    origin: String,
-    weight: {},
-    country_code: String,
-    height: {},
-    hypoallergenic: Number,
-    image: {
-        id: String,
-        width: String,
-        height: String,
-        url: String
-    }
-})
+    width: String,
+    height: String,
+    url: String,
+  },
+});
 // The User Schema
-const UserSchema = new Schema({
-    username: {type: String, unique: true, required: true},
-    password: {type: String, required: true},
+const UserSchema = new Schema(
+  {
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
     dogs: [Dog],
-}, {timestamps: true})
+  },
+  { timestamps: true }
+);
 // The User Model
-const User = model("User", UserSchema)
+const User = model("User", UserSchema);
 // Export the User Model
-module.exports = User
+module.exports = User;
